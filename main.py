3705da043ac6b2dca0d01dcd6b97c3c46ac6cec4
Arnaud Boudou <skuld@me.com>
2012-08-16 21:19:26 +0200

    Import initial

---
+++ main.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+import array
+import os
+import signal
+
+### Functions definition
+
+# Called on process interruption. Set all pins to "Input" default mode.
+def endProcess(signalnum = None, handler = None):
+    for gpioPinLed in gpioPinLedList:
+        GPIO.setup(gpioPinLed, GPIO.IN)
+    exit(0)
+
+# Put pins to out mode, and switch off LED.
+def switchOffLeds():
+    for gpioPinLed in gpioPinLedList:
+        GPIO.setup(gpioPinLed, GPIO.OUT)
+        GPIO.output(gpioPinLed, GPIO.LOW)
+
+ 
+### Main part
+
+# Get current pid
+pid = os.getpid()
+
+# Save current pid for later use
+try:
+    fhandle = open('/var/run/raspik2000.pid', 'w')
+except IOError:
+    print ("Unable to write /var/run/raspik2000.pid")
+    exit(1)
+fhandle.write(str(pid))
+fhandle.close()
+
+gpioPinLedList = [7, 11, 12, 13, 15, 16, 18, 22]
+sleeptime = 0.05
+
+# Prepare handlers for process exit
+signal.signal(signal.SIGTERM, endProcess)
+signal.signal(signal.SIGINT, endProcess)
+
+# Use Raspberry Pi board pin numbers
+GPIO.setmode(GPIO.BOARD)
+
+# Init output pins
+switchOffLeds()
+
+while True:
+
+    # Alternatively light on LED
+    loop = 0
+    while loop < 8:
+        time.sleep(sleeptime)
+        if loop == 0:
+            GPIO.output(gpioPinLedList[loop], GPIO.HIGH)
+        else:
+            GPIO.output(gpioPinLedList[loop], GPIO.HIGH)
+            GPIO.output(gpioPinLedList[loop-1], GPIO.LOW)
+        loop += 1
+
+    loop = 7
+    while loop > -1:
+        time.sleep(sleeptime)
+        if loop == 7:
+            GPIO.output(gpioPinLedList[loop], GPIO.HIGH)
+        else:
+            GPIO.output(gpioPinLedList[loop], GPIO.HIGH)
+            GPIO.output(gpioPinLedList[loop+1], GPIO.LOW)
+        loop -= 1 
---
+++ raspiledchaser.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# 
+# Start / Shutdown script for RasPiLEDChaser
+#
+
+DIR=$(cd $(dirname "$0"); pwd)
+
+case "$1" in
+  start)
+
+    # Start RasPiLEDChaser
+    `which python` "${DIR}"/main.py &
+
+    ;;
+  stop)
+    # Stop RasPiLEDChaser
+    `which kill` `cat /var/run/raspiledchaser.pd`
+    `which rm` /var/run/raspiledchaser.pid
+
+    ;;
+
+  *)
+    echo "Usage: $0 {start|stop}" >&2
+    exit 1
+
+    ;;
+
+esac
+
+exit 0
